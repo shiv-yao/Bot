57ef8e334e0ac442b58cc42338868ae23f090783
shiv-yao <153885823+shiv-yao@users.noreply.github.com>
2026-03-26 11:06:04 +0800

    Delete jito.py

--- jito.py
+++
@@ -1,9 +0,0 @@
-
-import aiohttp, os
-
-JITO = os.getenv("JITO_RPC")
-
-async def send_bundle(session, txs):
-    payload = {"jsonrpc":"2.0","id":1,"method":"sendBundle","params":[txs]}
-    async with session.post(JITO, json=payload) as r:
-        return await r.json()
