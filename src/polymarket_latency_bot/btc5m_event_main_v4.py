b33a69f93f93e1a4ade170ee8f9d6fea9eec540e
shiv-yao <153885823+shiv-yao@users.noreply.github.com>
2026-06-03 07:11:22 +0800

    feat: show BTC V4 selfcheck shortcut on mobile dashboard

--- src/polymarket_latency_bot/btc5m_event_main_v4.py
+++ src/polymarket_latency_bot/btc5m_event_main_v4.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from . import btc5m_event_main as legacy
 from .btc5m_analytics_v4 import build_paper_analytics as build_v4_paper_analytics
-from .btc5m_prediction_market_ui_v4 import register_btc5m_prediction_market_ui_v4
+from .btc5m_prediction_market_ui_v4_linked import register_btc5m_prediction_market_ui_v4
 from .btc5m_selfcheck import register_btc5m_selfcheck
 
 
